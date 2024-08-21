6faa050ed627df28d2472a62e67fab778be79fc5
Vinay4562 <manikantivinaykumar@yahoo.com>
2024-08-21 22:34:33 +0530

    Initial commit

--- server.js
+++ server.js
@@ -51,15 +51,15 @@ passport.deserializeUser((username, done) => {
 });
 
 const credentials = {
-    "220/132/33KV Tandur": { username: "tandurUser", password: "tandurPass" },
-    "220KV SS Chandanavally": { username: "chandanavallyUser", password: "chandanavallyPass" },
-    "132/33KV Kodangal": { username: "kodangalUser", password: "kodangalPass" },
-    "132/33KV Kanakamamidi": { username: "kanakamamidiUser", password: "kanakamamidiPass" },
-    "132/33KVSS Parigi": { username: "parigiUser", password: "parigiPass" },
-    "132/33KVSS Puttapahad": { username: "puttapahadUser", password: "puttapahadPass" },
-    "132/33KVSS SRIRANGAPUR": { username: "srirangapurUser", password: "srirangapurPass" },
-    "132/33KVSS Vikarabad": { username: "vikarabadUser", password: "vikarabadPass" },
-    "132/33KV Donthanpally": { username: "donthanpallyUser", password: "donthanpallyPass" }
+    "220/132/33KV Tandur": { username: "tandur220kv", password: "tandur@V514" },
+    "220KV SS Chandanavally": { username: "chandanavally220kv", password: "chandanavally@V168" },
+    "132/33KV Kodangal": { username: "kodangal132kv", password: "kodangal@V784" },
+    "132/33KV Kanakamamidi": { username: "kanakamamidi132kv", password: "kanakamamidi@V642" },
+    "132/33KVSS Parigi": { username: "parigi132kv", password: "parigi@V326" },
+    "132/33KVSS Puttapahad": { username: "puttapahad132kv", password: "puttapahad@V198" },
+    "132/33KVSS  SRIRANGAPUR": { username: "srirangapur132kv", password: "srirangapur@V446" },
+    "132/33KVSS  Vikarabad": { username: "vikarabad132kv", password: "vikarabad@V156" },
+    "132/33KV Donthanpally": { username: "donthanpally132kv", password: "donthanpally@V848" }
 };
 
 app.get('/interruption.html', ensureAuthenticated, (req, res) => {

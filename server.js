80cf2c1d414d22dfb6bf4b6c11e16a2ab2574869
Vinay4562 <manikantivinaykumar@yahoo.com>
2024-08-21 19:08:49 +0530

    Initial commit

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, 'public')));
+pp.use(express.static(path.join(__dirname, 'public')));
 
-// Security Middleware
-app.use(helmet());
-
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 .then(() => console.log('MongoDB connected...'))
 .catch(err => console.error('Database connection error:', err));
 
